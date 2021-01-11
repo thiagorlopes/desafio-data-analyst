bf7aa3be0752178a5732c460a4f5702522eb2b1c
thiago.rlopes <thiago.rlopes@outlook.com>
2021-01-10 22:52:13 -0300

    Finish Conexa data analyst challenge.
    * Create extract_top helper function for returning first n items sorted
    in descending order;
    * Include helper function for plotting bar plots and enabling code
    reusability;
    * Insert helper function for annotating bars with value;
    * Include visualizations for data;
    * Finish conclusion.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,28 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
+# Imprime valores únicos, exceto nulos, para uma dada série pandas
 def print_unique(series):
     unique_values = np.unique(series.dropna())
     print("Total: ", len(unique_values))
     print(unique_values)
+
+# Extrai os n primeiros itens para uma coluna específica do dataframe
+def extract_top(df, n, column):
+    return df.sort_values(by=column, ascending=False).head(n)
+
+# Plota um gráfico de barras para um dado dataframe
+def plot_bars(df, x, y):
+    return sns.catplot(data=df, x=x, y=y, kind='bar', height=5, aspect=2)
+
+# Insere valores acima de um gráfico de barras
+def annotate_bars(g):
+    for ax in g.axes.ravel():
+        for p in ax.patches:
+            ax.annotate(format(p.get_height(), '.2f'),
+                (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha = 'center',
+                va = 'center',
+                xytext = (0, 10),
+                textcoords = 'offset points')
